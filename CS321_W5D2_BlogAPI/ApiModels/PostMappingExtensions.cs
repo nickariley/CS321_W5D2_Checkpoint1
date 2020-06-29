a9c81e38521f481035b72d33174b3528d7a89474
nickariley <nickariley@gmail.com>
2020-06-29 17:43:28 -0500

    Updated PostMappingExtensions, mapped AuthorName and BlogName

--- CS321_W5D2_BlogAPI/ApiModels/PostMappingExtensions.cs
+++ CS321_W5D2_BlogAPI/ApiModels/PostMappingExtensions.cs
@@ -16,9 +16,11 @@ namespace CS321_W5D2_BlogAPI.ApiModels
 				Title = post.Title,
 				Content = post.Content,
 				CommentsAllowed = post.CommentsAllowed,
-                BlogId = post.BlogId,
-                DatePublished = post.DatePublished,
-                // TODO: map blogName and authorName
+				BlogId = post.BlogId,
+				DatePublished = post.DatePublished,
+				// TODO: map blogName and authorName
+				AuthorName = post.Blog.User.FullName,
+				BlogName = post.Blog.Name
 			};
 		}
 

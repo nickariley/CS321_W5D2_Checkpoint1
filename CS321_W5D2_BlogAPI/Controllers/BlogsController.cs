0ae9f153f6142922122234706f70b84b87da7861
nickariley <nickariley@gmail.com>
2020-07-01 20:57:04 -0500

    Impemented GetAll and Get(id) in BlogsController

--- CS321_W5D2_BlogAPI/Controllers/BlogsController.cs
+++ CS321_W5D2_BlogAPI/Controllers/BlogsController.cs
@@ -19,8 +19,9 @@ namespace CS321_W5D2_BlogAPI.Controllers
         private readonly IBlogService _blogService;
 
         // TODO: inject BlogService
-        public BlogsController()
+        public BlogsController(IBlogService blogService)
         {
+            _blogService = blogService;
         }
 
         // GET: api/blogs
@@ -32,15 +33,12 @@ namespace CS321_W5D2_BlogAPI.Controllers
             {
                 // TODO: replace the code below with the correct implementation
                 // to return all blogs
-                return Ok(new BlogModel[] {
-                    new BlogModel
-                    {
-                        Id = 1,
-                        Name = "Fix Me!",
-                        Description = "Implement GET /api/blogs",
-                        AuthorName = "unknown",
-                    }
-                });
+
+                var allBlogs = _blogService
+                    .GetAll()
+                    .ToApiModels();
+
+                return Ok(allBlogs);
             }
             catch (Exception ex)
             {
@@ -58,13 +56,12 @@ namespace CS321_W5D2_BlogAPI.Controllers
             {
                 // TODO: replace the code below with the correct implementation
                 // to return a blog by id
-                return Ok(new BlogModel
-                {
-                    Id = id,
-                    Name = "Fix Me!",
-                    Description = "Implement GET /api/blogs/{id}",
-                    AuthorName = "unknown",
-                });
+                var currentBlog = _blogService.Get(id);
+
+                if (currentBlog == null) return null;
+
+                return Ok(currentBlog.ToApiModel());
+
             }
             catch (Exception ex)
             {
